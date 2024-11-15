616b0dc5a93a18b51191f465a1539290c4752917
unknown <frankpythagore@gmail.com>
2024-11-15 13:35:34 +0100

    Project initialize with h2 test db and postgres db

---
+++ src/main/java/com/fly/tradihub/TradihubApplication.java
@@ -0,0 +1,13 @@
+package com.fly.tradihub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradihubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradihubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fly/tradihub/TradihubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fly.tradihub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradihubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
